8ed397cebfd45e4c26cc5ae9359a65dfe3a8ad26
DarioRoman <darioroman2001@gmail.com>
2021-04-07 19:34:03 -0400

    refactoring code.

--- server/utils/validation.go
+++ server/utils/validation.go
@@ -9,39 +9,19 @@ import (
 // validate all the fields in the users registration
 func ValidateRegister(options models.UserInput) *models.UserResponse {
 	if strings.Contains(options.Username, "@") {
-		return &models.UserResponse{
-			Error: &models.FieldError{
-				Field:   "username",
-				Message: "@ not allowed in username",
-			},
-		}
+		return GenUserResponseError("username", "@ not allowed in username")
 	}
 
 	if len(options.Username) < 3 {
-		return &models.UserResponse{
-			Error: &models.FieldError{
-				Field:   "username",
-				Message: "username must be at least 3 characters",
-			},
-		}
+		return GenUserResponseError("username", "username must be at least 3 characters")
 	}
 
 	if !strings.Contains(options.Email, "@") || !strings.Contains(options.Email, ".") {
-		return &models.UserResponse{
-			Error: &models.FieldError{
-				Field:   "email",
-				Message: "Invalid email",
-			},
-		}
+		return GenUserResponseError("email", "invalid email")
 	}
 
 	if len(options.Password) <= 3 {
-		return &models.UserResponse{
-			Error: &models.FieldError{
-				Field:   "password",
-				Message: "Password must be at least 4 characters",
-			},
-		}
+		return GenUserResponseError("password", "Password must be at least 4 characters")
 	}
 
 	return nil
